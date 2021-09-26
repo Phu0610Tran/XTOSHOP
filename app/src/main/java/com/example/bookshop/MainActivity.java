0677e60eb6a61afc95b6f499f06c7cd410c34a03
Phu0610Tran <phudodanh156@gmail.com>
2021-09-26 21:13:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bookshop/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.bookshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTabLayout = findViewById(R.id.tablayoutMain);
+        mViewPager = findViewById(R.id.ViewpagerMain);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        mViewPager.setAdapter(viewPagerAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
+    }
+}
